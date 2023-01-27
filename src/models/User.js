afdf24663e1aaaf8d7b4e7308c61606110f06766
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-01-27 15:06:59 -0300

    feat: completa requisitpo 2(cria model de User)

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,7 @@ const sequelize = require("sequelize");
 
 const UserModel = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
+        id: { type: DataTypes.INTEGER, primaryKey: true },
         display_name: DataTypes.STRING,
         email: DataTypes.STRING,
         password: DataTypes.STRING,
